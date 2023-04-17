c36f3c44db51b98d53eafa1cd000af5d53d43ec7
offlineProgrammer <mmalaka@gmail.com>
2023-04-17 11:17:24 -0700

    Clean up

--- lib/ui/pages/memory_match_page_mobile.dart
+++ lib/ui/pages/memory_match_page_mobile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:the_memory_match_game/ui/widgets/game_board.dart';
-
 import 'package:the_memory_match_game/utils/constants.dart';
 
 class MemoryMatchPageMobile extends StatelessWidget {
